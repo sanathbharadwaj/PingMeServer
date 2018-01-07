2bef4a7ab5dd14572bbd4f217beb1912b6696641
cd healthyTOD <sanath.bharadwaj21@gmail.com>
2018-01-07 18:20:41 +0530

    Updated cloud code

--- cloud/main.js
+++ cloud/main.js
@@ -11,8 +11,8 @@ Parse.Cloud.define('sendNotification', function(req, res){
 	Parse.Push.send({
   	where: query,
   	data: {
-  		title : 1,
-    	alert: req.params.alertId
+  		title : "New Ping",
+    	alert: req.params.mobileNo
   		}
 }, {
 	useMasterKey: true,
@@ -24,34 +24,6 @@ Parse.Cloud.define('sendNotification', function(req, res){
   error: function(error) {
     // Handle error
     res.error("Failed to send the push");
-  }
-});
-});
-
-
-Parse.Cloud.define('newRequest', function(req, res){
-	var userQuery = new Parse.Query(Parse.User);
-	userQuery.equalTo("isDriver", true);
-
-	var pushQuery = new Parse.Query(Parse.Installation);
-	pushQuery.matchesQuery('user', userQuery);
-
-	Parse.Push.send({
-  		where: pushQuery,
-  	data: {
-    alert: "New request has arrived",
-    title: "New Request"
-  }
-}, {
-	useMasterKey: true,
-
-  success: function() {
-    // Push was successful
-    res.success(1);
-  },
-  error: function(error) {
-    // Handle error
-    res.error("Push failed");
   }
 });
 });
