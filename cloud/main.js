d4ffc65eb488a7e373f5cce23070654c73158f73
cd healthyTOD <sanath.bharadwaj21@gmail.com>
2018-01-11 21:38:13 +0530

    Updated cloud code for firebase implementation

--- cloud/main.js
+++ cloud/main.js
@@ -4,7 +4,25 @@ Parse.Cloud.define('hello', function(req, res) {
 });
 
 Parse.Cloud.define('sendNotification', function(req, res){
-	var installationId = req.params.installation;
+	var userId = req.params.userId;
+	var firebaseUser = Parse.Object.extend("FirebaseUser");
+	var query = new Parse.Query(firebaseUser);
+	query.equalTo("userId",userId);
+	query.limit(1);
+	query.find({
+		 success: function(results) {
+    // Do something with the returned Parse.Object values
+    var object = results[0];
+    var id = object.get("insId");
+    pushNotification(id);
+  },
+  error: function(error) {
+    alert("Error: " + error.code + " " + error.message);
+  }
+	});
+
+
+function pushNotification(installationId){
 	var query = new Parse.Query(Parse.Installation);
 	query.equalTo("installationId", installationId);
 
@@ -26,4 +44,5 @@ Parse.Cloud.define('sendNotification', function(req, res){
     res.error("Failed to send the push");
   }
 });
+}
 });
