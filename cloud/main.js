11612251389131b9bcb9e686eac71e83e5508e72
cd healthyTOD <sanath.bharadwaj21@gmail.com>
2018-02-10 13:38:48 +0530

    Added started trip notification

--- cloud/main.js
+++ cloud/main.js
@@ -22,7 +22,7 @@ Parse.Cloud.define('sendNotification', function(req, res){
 	});
 
 
-function pushNotification(installationId){
+function pushNotification(installationId, req){
 	var query = new Parse.Query(Parse.Installation);
 	query.equalTo("installationId", installationId);
 	console.log("My alert:" + req.params.title);
@@ -46,6 +46,52 @@ function pushNotification(installationId){
 });
 }
 });
+
+Parse.Cloud.define('tripStartedNotification', function(req, res){
+  var userIds = req.params.userIds;
+  var firebaseUser = Parse.Object.extend("FirebaseUser");
+  var query = new Parse.Query(firebaseUser);
+  query.containedIn("userId",userIds);
+  query.find({
+     success: function(results) {
+    //var id = object.get("insId");
+    pushNotification(results);
+  },
+  error: function(error) {
+    res.error(0);
+  }
+  });
+
+
+function pushNotification(results){
+  var insIds = [];
+  for (var i = results.length - 1; i >= 0; i--) {
+    insIds[i] = results[i].get("insId");
+  }
+  var driverId  = req.params.extras[0];
+  var query = new Parse.Query(Parse.Installation);
+  query.containedIn("installationId", insIds);
+  Parse.Push.send({
+    where: query,
+    data: {
+      title : "beginTrip",
+      alert: driverId
+      }
+}, {
+  useMasterKey: true,
+
+  success: function() {
+    // Push was successful
+    res.success(1);
+  },
+  error: function(error) {
+    // Handle error
+    res.error(0);
+  }
+});
+}
+});
+
 
 Parse.Cloud.define('notificationToAll', function(req, res){
 	var query = new Parse.Query(Parse.Installation);
